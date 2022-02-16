4e72925b707232be16ec2c75bc720504dcea1de2
mni-dev <nazrulislam01531@gmail.com>
2022-02-17 03:20:34 +0600

    saving amount and remaining balance fixed

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+/* -------------------------------------------------
+                Income and Expenses Section
+-------------------------------------------------- */
 document.getElementById('btn-calculate').addEventListener('click', function(){
     const incomeInput = document.getElementById('income');
     // console.log(incomeInput)
@@ -29,14 +32,25 @@ document.getElementById('btn-calculate').addEventListener('click', function(){
     balance.innerText = balanceAmount;
      
 })
-document.getElementById('btn-save').addEventListener('click', function(){
+/*---------------------------------------------------------
+                        Saving Section
+----------------------------------------------------------*/
+function savings(){
     const incomeInput = document.getElementById('income');
     const incomeAmount = incomeInput.value;
-    const saveInput = document.getElementById('save');
-    const saveAmount = saveInput.value;
-    const savingAmount = document.getElementById('saving-amount');
-    savingAmount.innerText = parseFloat(incomeAmount) / parseFloat(saveAmount);
-    // console.log(saveAmount);
+    // incomeInput.value = parseFloat(incomeAmount);
+    // console.log(incomeAmount);
+    const savingPercentage = document.getElementById('save');
+    const savingAmount = savingPercentage.value;
+    // savingPercentage.value = parseFloat(savingAmount)
     // console.log(savingAmount);
-
-}) 
+    //Saving Amount
+    const savingCalc = (incomeAmount * savingAmount) / 100;
+    // console.log(savingCalc);
+    const savings =document.getElementById('saving-amount');
+    savings.innerText = savingCalc;
+    //Remaining Balance
+    const balance = document.getElementById('balance');
+    const remainingBalance = document.getElementById('remaining-balance');
+    remainingBalance.innerText = balance.innerText - savings.innerText;
+}
