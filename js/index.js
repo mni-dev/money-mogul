fa9467488b2b2c2d4f7bbda682ca71b5408579a4
mni-dev <nazrulislam01531@gmail.com>
2022-02-17 22:56:21 +0600

    js coding finalized except error handling

--- js/index.js
+++ js/index.js
@@ -2,31 +2,29 @@
                 Income and Expenses Section
 -------------------------------------------------- */
 document.getElementById('btn-calculate').addEventListener('click', function(){
+    //Income Amount
     const incomeInput = document.getElementById('income');
-    // console.log(incomeInput)
     const incomeAmount = incomeInput.value;
-    // console.log(incomeAmount)
     incomeInput.value = incomeAmount;
-    // console.log(incomeAmount);
+    //Fost Cost
     const foodInput = document.getElementById('food');
     const foodCost = foodInput.value;
-    // console.log(foodCost);
     foodInput.value = foodCost;
-    // console.log(foodCost);
-
+    //Rent Cost
     const rentInput = document.getElementById('rent');
     const rentCost = rentInput.value;
     rentInput.value = rentCost;
-    // console.log(rentCost)
+    //Clothes Cost
     const clothesInput = document.getElementById('clothes');
     const clothesCost = clothesInput.value;
     clothesInput.value = clothesCost;
-    // console.log(clothesCost)
-    //Total Expenses 
+
+    //Total Expenses/Cost 
     const expenses = document.getElementById('total-expense');
     const totalExpenses = parseFloat(foodCost) + parseFloat(rentCost) + parseFloat(clothesCost);
     expenses.innerText = totalExpenses;
-    console.log(totalExpenses)
+     
+
     //Balance after expenses
     const balance = document.getElementById('balance');
     const balanceAmount = parseFloat(incomeAmount) - totalExpenses;
@@ -39,18 +37,15 @@ document.getElementById('btn-calculate').addEventListener('click', function(){
 function savings(){
     const incomeInput = document.getElementById('income');
     const incomeAmount = incomeInput.value;
-    // incomeInput.value = parseFloat(incomeAmount);
-    // console.log(incomeAmount);
     const savingPercentage = document.getElementById('save');
     const savingAmount = savingPercentage.value;
-    // savingPercentage.value = parseFloat(savingAmount)
-    // console.log(savingAmount);
+     
     //Saving Amount
     const savingCalc = (incomeAmount * savingAmount) / 100;
-    // console.log(savingCalc);
     const savings =document.getElementById('saving-amount');
     savings.innerText = savingCalc;
-    //Remaining Balance
+
+    //Remaining Balance after saving
     const balance = document.getElementById('balance');
     const remainingBalance = document.getElementById('remaining-balance');
     remainingBalance.innerText = balance.innerText - savings.innerText;
